a9806301f36073d284f24623d145337dc3f71b55
Maxson <93727813+v-maxson@users.noreply.github.com>
2023-07-07 19:19:24 -0600

    feat: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,21 @@ mod context; pub use context::*;
 mod error; pub use error::*;
 pub mod subsystems;
 pub mod version;
+
+#[cfg(test)]
+mod tests {
+    use crate::{SdlContext, SdlError};
+
+
+    #[test]
+    fn timer_test() -> Result<(), SdlError> {
+        let ctx = SdlContext::new()?;
+        let timer = ctx.timer()?;
+
+        timer.delay(3000);
+
+        println!("Time Since SdlContext initialization: {}ms", timer.get_ticks());
+
+        Ok(())
+    }
+}
