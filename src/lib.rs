205201ca87c11589b9c535d4b7a80b4d99f4e3cc
Maxson <93727813+v-maxson@users.noreply.github.com>
2023-07-07 21:18:05 -0600

    feat: remove tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,27 +15,3 @@ mod context; pub use context::*;
 mod error; pub use error::*;
 pub mod subsystems;
 pub mod version;
-
-#[cfg(test)]
-mod tests {
-    use ctor::ctor;
-    use simplelog::{SimpleLogger, Config};
-    use crate::{SdlContext, SdlError};
-
-    // This ensures there is a logger initialized for use with the `log` crate in all tests. 
-    // You should NOT use the `ctor` crate with anything that calls into Rust's standard library.
-    #[ctor]
-    static LOGGER: Box<SimpleLogger> = SimpleLogger::new(log::LevelFilter::Off, Config::default());
-
-    #[test]
-    fn timer_test() -> Result<(), SdlError> {
-        let ctx = SdlContext::new()?;
-        let timer = ctx.timer()?;
-
-        timer.delay(3000);
-
-        println!("Time Since SdlContext initialization: {}ms", timer.get_ticks().as_millis());
-
-        Ok(())
-    }
-}
